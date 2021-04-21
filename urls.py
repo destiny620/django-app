74ae67565e5e3d4d136169a9ef99b77c76543429
destiny620 <81220100+destiny620@users.noreply.github.com>
2021-04-21 19:07:47 +0100

    main urls

--- urls.py
+++ urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index')
+   path('myapps/', include('myapps.urls')), 
+   path('admin/', admin.site.urls)
 ]
+
